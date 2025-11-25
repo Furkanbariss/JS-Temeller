4bcad8a9c6fc2ba9c245254c7f15f62a9a662e38
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-11-25 22:59:34 +0300

    factorial calcuate added

---
+++ faktoriyel hesaplama/app.js
@@ -0,0 +1,7 @@
+
+let input=Number(prompt("lütfen faktoriyelinin hesaplanmasını istediğiniz sayıyı giriniz:"));
+let result=1;
+for(let i=1;i<=input;i++){
+    result=result*i;
+}
+alert("faktöriyel sonucunuz:"+result);
