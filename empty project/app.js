e68b9a98b9e4d8636a4b64e6fba40761dc466efb
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-11-29 21:31:11 +0300

    empty project added

---
+++ empty project/app.js
@@ -0,0 +1,28 @@
+//Müsvette alan
+// let myNumbers= [0,1,2,3,4,5,6,7,8,9];
+
+// myNumbers.push("a");
+
+// myNumbers.pop();
+ // myNumbers.shift();
+
+// myNumbers.unshift("b");
+
+// myNumbers.splice(1,3,"araba");
+
+// let myName=["Furkan","Barış","Sönmezışık"];
+
+// console.log(myName.toString());
+
+// let index= myName.indexOf("Barış");
+// console.log("-----------");
+// console.log(index);
+// console.log("-----------");
+
+// myNumbers.forEach(function (number){
+//     console.log(number);
+    
+// });
+
+//-----------------------------------------------------------------------
+
