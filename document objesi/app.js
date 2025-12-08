4da0908e2fe7e5ba90ecdacfdac0f3689b40a59f
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-08 20:27:22 +0300

    document objesi part 1

---
+++ document objesi/app.js
@@ -0,0 +1,44 @@
+//-----------------------------------------------------ebeveyn çocuk arasında gezinme--------------------------------------------------------------
+
+// const row = document.querySelector(".row");
+// let list = row.children[0].children[3].children[0];
+// list.textContent="deneme2";
+// let todo3 = row.children[0].children[3].children[2].children[2];
+// list.style.backgroundColor ="yellow";
+// // todo3.style.backgroundColor ="yellow";
+// let todoString;
+// // todoString.style.backgroundColor="yellow"
+
+// console.log(list);
+// console.log(todo3);
+// console.log(todoString);
+
+
+//--------------------------------------------------------elemnet oluşturma----------------------------------------------------------------
+/*
+<li class="list-group-item d-flex justify-content-between">Todo 2
+                            <a href="#" class="delete-item">
+                                <i class="fa fa-remove"></i>
+                            </a>
+</li>
+*/
+
+// const listAdd = document.querySelector(".list-group");
+
+// let todoAdd = document.createElement("li");
+// todoAdd.className="list-group-item d-flex justify-content-between";
+// todoAdd.innerHTML="Todo 5";
+
+// let todolink = document.createElement("a");
+// todolink.href="#";
+
+// let todoicon = document.createElement("i");
+// todoicon.className="fa fa-remove";
+
+// todolink.appendChild(todoicon);
+// todoAdd.appendChild(todolink);
+// listAdd.appendChild(todoAdd);
+
+// console.log(todoAdd);
+
+//--------------------------------------------------------------element silme----------------------------------------------------------------
