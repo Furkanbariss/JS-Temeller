4646b29824883b2c4d5db450f7831b899a5f0cbf
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-09 17:08:52 +0300

    events part 1 - addEventListener

---
+++ document objesi/app2.js
@@ -0,0 +1,17 @@
+
+/*
+const clearAll = document.querySelector("#todoClearButton");
+
+clearAll.addEventListener("click",deleteLastItem);
+
+// const todo = document.querySelector(".list-group");
+function deleteLastItem(e){
+// todo.remove();
+document.querySelector(".list-group").remove();
+console.log(e.type);
+console.log(e.target);
+console.log(e.target.textContent +" butonu çalıştı");
+}
+
+// console.log(todo);
+*/
