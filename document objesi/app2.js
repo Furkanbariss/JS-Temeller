ff54d3c1f50a3b078a9df7178cdb68eccd1c8443
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-09 20:51:51 +0300

    events part 2

--- document objesi/app2.js
+++ document objesi/app2.js
@@ -15,3 +15,39 @@ console.log(e.target.textContent +" butonu çalıştı");
 
 // console.log(todo);
 */
+//------------------------------------------------mouse eventları--------------------------------------------------------
+
+//yazmaya gerek duymadım
+
+
+//------------------------------------------------Klavye eventları--------------------------------------------------------
+
+//keypress: harf ve sayıları algılar.
+
+//keydown: her şeyi algılar
+
+//keyup: tuştan elini çekince algılar
+
+// document.addEventListener("keydown",run);
+
+// // function run(e){
+// // console.log(e.key);
+// // }
+
+// function run(e){
+//     console.log(e.keyCode);
+//     if(e.keyCode ==116){
+//         alert("Sayfa yenileme yapamazsınız!")
+//     }
+//     e.preventDefault();
+// }
+
+const baslik = document.querySelectorAll(".card-title")[0];
+const input = document.querySelector("#todoName");
+
+input.addEventListener("keydown",run);
+
+
+function run(e){
+    baslik.textContent = e.target.value;
+}
