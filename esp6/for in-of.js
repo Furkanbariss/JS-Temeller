eb44ca5ed535d8fcade067872531309b69758517
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-14 00:42:35 +0300

    es6 part 2

---
+++ esp6/for in-of.js
@@ -0,0 +1,14 @@
+let mysiblings = ["ahmet", "ibrahim", "Abdulkadir"];
+
+// for in : belirtilen dizinin elemanlarının sırayla indexlerini döner.
+
+for (let print1 in mysiblings) {
+  console.log(print1);
+}
+
+console.log("---------------------");
+// for of :belirtilen dizinin elemanlarının sırayla değerlerini döner.
+
+for (let print2 of mysiblings) {
+  console.log(print2);
+}
