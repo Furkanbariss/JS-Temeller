03bbafded848a805b30479f0b01d0f78db467fc0
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-15 15:14:30 +0300

    oop last part

--- OOP/app.js
+++ OOP/app.js
@@ -3,6 +3,8 @@ class araba {
   // 2- yapıcı metod
   // 3- fonksiyonlar
 
+  static tekerlekSayisi = 4;
+
   constructor(motor, renk, vites, sunroof) {
     // yapıcı metod
     this.motor = motor;
@@ -20,3 +22,4 @@ class araba {
 
 let tiugan = new araba("1.5 benzinli", "pembe", "otomatik", "yok");
 tiugan.yazdir();
+console.log(araba.tekerlekSayisi); // staticler class'a tanımlanan bir şeydir. yani class üzerinden tanımlanmış objelere statik özellikler geçmez. bu yüzden class üzerinden çağrılırlar.
