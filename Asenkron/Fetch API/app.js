af641470cd399c289b51bfd7bafab80954198f03
Furkan Barış Sönmezışık <sonmezisikfurkanbaris@gmail.com>
2025-12-16 18:10:37 +0300

    asycn await

--- Asenkron/Fetch API/app.js
+++ Asenkron/Fetch API/app.js
@@ -1,8 +1,34 @@
-function getUsersData(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-}
+// function getUsersData(url) {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// }
 
-getUsersData("https://jsonplaceholder.typicode.com/users");
+// getUsersData("https://jsonplaceholder.typicode.com/users");
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+document.querySelector("#button").addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((data) =>
+      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${data.id}`)
+        .then((response) => response.json())
+        .then((allpostdata) => console.log(allpostdata))
+    )
+    .catch((err) => console.log(err));
+});
+
+//! asycn-await ile yapımı
+document.querySelector("#button2").addEventListener("click", async () => {
+  const post = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  ).json();
+  const comments = await (
+    await fetch(
+      `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`
+    )
+  ).json();
+  console.log(post, comments);
+});
